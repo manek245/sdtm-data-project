81547b9d3115fee4c9b1fbd9e0125a613cb3e276
Mane Karapetyan <mane.karapetyan@armatanalytics.com>
2026-05-08 18:53:11 +0400

    Adding more data to EX.

--- EX.R
+++ EX.R
@@ -1,15 +1,26 @@
+first_dose <- aggregate(infdate ~ patid, dosing_df, min)
+dosing_min <- merge(dosing_df,
+                    first_dose,
+                    by = "patid",
+                    suffixes = c("", "_first"))
+
 ex <- data.frame(
   STUDYID = dosing_df$study,
   DOMAIN = "EX",
-  USUBJID = paste0(dosing_df$study, substr(dosing_df$patid, 4, nchar(dosing_df$patid))),
+  USUBJID = paste0(dosing_df$study, substr(
+    dosing_df$patid, 4, nchar(dosing_df$patid)
+  )),
   EXSEQ = ave(seq_along(dosing_df$patid), dosing_df$patid, FUN = seq_along),
   EXTRT = dosing_df$rand,
   EXDOSE = as.numeric(gsub("[^0-9.]", "", dosing_df$dose)),
   EXDOSEU = ifelse(is.na(dosing_df$dose), NA, 'ml'),
-  EXSTDTC = NA,
-  EXENDDTC = NA
-  
+  EXADJ = dosing_df$intreas,
+  EXSTDTC = to_iso_ymd_hms(dosing_df$infdate, dosing_df$infsttm),
+  EXENDDTC = to_iso_ymd_hms(dosing_df$infdate, dosing_df$infentm),
+  EXSTDY = paste("Day",get_day(dosing_min$infdate_first, dosing_min$infdate)
+  )
 )
 
 
-View(ex)
+writexl::write_xlsx(ex,
+                    "C:\\Users\\ManeKarapetyan\\Desktop\\sdtm-data-project\\ex.xlsx")
